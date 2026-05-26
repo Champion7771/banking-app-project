c07373bae495c10d59a28c94f516028782258592
Champion7771 <akashrainaf5@gmail.com>
2026-05-26 11:24:55 +0530

    fixed api production fallback

--- Frontend/vite-project/src/api/axios.ts
+++ Frontend/vite-project/src/api/axios.ts
@@ -1,20 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://banking-app-project.onrender.com/api",
+
   withCredentials: true,
 });
 
 api.interceptors.response.use(
-  // SUCCESS RESPONSE
   (response) => response,
 
-  // ERROR RESPONSE
   async (error) => {
-    // TEMPORARY:
-    // refresh token logic removed
-    // because backend /auth/refresh route does not exist yet
-
     return Promise.reject(error);
   },
 );
